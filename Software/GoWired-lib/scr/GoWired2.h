836df6243363c687b3c6584340218d3a454aae53
Feanaro <feanor.anglin@gmail.com>
2022-04-29 16:22:16 +0200

    Adjust GoWired-lib folders, edit library.properties

---
+++ Software/GoWired-lib/scr/GoWired2.h
@@ -0,0 +1,19 @@
+/**
+ * @file GoWired2.h
+ * @author Jakub Kazibudzki (hello@gowired.dev)
+ * @brief This file combines headers used in GoWired DIN-mountable modules (Software/8RelayDin & Software/Heating Controller)
+ * @version 1.0
+ * @date 2022-04-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef GoWired2_h
+#define GoWired2_h
+
+#include "core/CommonIO.h"
+#include "core/ExpanderIO.h"
+#include "core/Heating.h"
+
+#endif
